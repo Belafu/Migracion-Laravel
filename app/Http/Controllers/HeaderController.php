0cc074c829d05e7d51c06f75b50d9125f390abf7
belafu <gyupanquisalvatierra@gmail.com>
2019-07-26 11:36:45 -0300

    corrigo vista de losl productos

--- app/Http/Controllers/HeaderController.php
+++ app/Http/Controllers/HeaderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Product;
 use App\Tag;
+use App\Cart;
 
 class HeaderController extends Controller
 {
@@ -16,11 +17,14 @@ class HeaderController extends Controller
      public function search(Request $request)
      {
        $tags = Tag::all();
+       $carritoActual = Cart::where('status','=',0)->get();
+       $carritoActual = $carritoActual->map(function($elem){return $elem->name;});
        //$carritoActual = [];
        $param = $request['search'];
 
+
        $products = Product::where('name', 'like', "%$param%" )->get(); //no olvidar get() para las consultas where;
        //$products->withPath('?search=' . $param);
-       return view('products', compact('products','tags'));
+       return view('products', compact('products','tags','carritoActual'));
      }
 }
