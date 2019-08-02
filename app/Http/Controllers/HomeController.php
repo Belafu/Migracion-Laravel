7a7243fe36a2fdf8214273b931f1aaf64215b557
gianpieryup <gyupanquisalvatierra@gmail.com>
2019-08-02 18:03:14 -0300

    sfd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Product;
+use App\Product_tag;
 class HomeController extends Controller
 {
     /**
@@ -23,6 +24,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+      $mouse = Product_tag::where('tag_id','=',1)->first();
+      $mouse = Product::find($mouse->product_id);
+      $teclado = Product_tag::where('tag_id','=',2)->first();
+      $teclado = Product::find($teclado->product_id);
+      $auricular = Product_tag::where('tag_id','=',3)->first();
+      $auricular = Product::find($auricular->product_id);
+    //  dd($mouse,$teclado,$auricular);
+        return view('index',compact('mouse','teclado','auricular'));
     }
 }
