fc67390420611dc8615766efe501c6728644f4d4
Francodiner <francodiner1232011@gmail.com>
2019-08-02 13:24:18 -0300

    sigo corrigiendo vistas

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,21 +4,30 @@
 
 @section('content')
 
-<h2>Detalle {{$product->name}}</h2>
 
-        <article class="">
-          <p>Nombre: {{$product->name}}</p>
-          <p>Descripción: {{$product->description}}</p>
-          <p>Precio: {{$product->price}}</p>
 
-          <form class="" action="/product/{id}" method="post">
-            @csrf
-            <input type="hidden" name="id" value="{{$product->id}}">
-            <button type="submit"> borrar </button>
-          </form>
 
-          <img src="/storage/products/{{$product->featured_img}}" alt="">
-          <a href="/product/addtocart/{{$product->id}}">Agrerar al carrito</a>
+        <article class="info-productos infooo">
+
+          <div class="foto-producto">
+            <img src="/storage/products/{{$product->featured_img}}" alt="">
+          </div>
+
+          <div class="info-producto">
+            <h2>Detalle {{$product->name}}</h2>
+            <p>Nombre: {{$product->name}}</p>
+            <p>Descripción: {{$product->description}}</p>
+            <p>Precio: {{$product->price}}</p>
+
+            <form class="" action="/product/{id}" method="post">
+              @csrf
+              <input type="hidden" name="id" value="{{$product->id}}">
+              {{-- <button type="submit"> borrar </button> --}}
+            </form>
+
+            <a href="/product/addtocart/{{$product->id}}"><button class="btn btn-dark" type="submit" name="button">Agregar al carrito</button></a>
+          </div>
+
         </article>
 
 
