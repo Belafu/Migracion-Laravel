b490ab2d88e3d09663121a7961386acb41aa4eca
gianpieryup <gyupanquisalvatierra@gmail.com>
2019-08-02 18:07:57 -0300

    sfd

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/', 'HomeController@index');
+// Route::get('/', 'HomeController@index');
 Route::get('/product-add', 'ProductController@create');
 Route::post('/product-add', 'ProductController@store');
 
